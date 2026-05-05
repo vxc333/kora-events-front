83345401c8e42199d77ba7e6c6cc4028ec1916a2
Vitor Xavier <vitor.xavier.dev@gmail.com>
2026-05-05 18:24:31 -0300

    fix: Corrige a importação do tipo DragEndEvent do @dnd-kit/core

--- src/components/PageBuilderTab.tsx
+++ src/components/PageBuilderTab.tsx
@@ -5,8 +5,8 @@ import {
   PointerSensor,
   useSensor,
   useSensors,
-  DragEndEvent,
 } from '@dnd-kit/core'
+import type { DragEndEvent } from '@dnd-kit/core'
 import {
   SortableContext,
   verticalListSortingStrategy,
